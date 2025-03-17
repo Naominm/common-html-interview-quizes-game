a68a269164c34e8ef5756a2ad74c6f506598c571
Naominm <naomimbugua536@gmail.com>
2025-03-17 12:16:56 +0300

    Getting first question

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -6,10 +6,19 @@ import "./Game.css";
 function Game(){
     const[questions,setQuestions]=useState([]);
 
+    const[currentQuestionIndex,setCurrentQuestionIndex]=useState(0);
+
+    const [error,setError]=useState("")
+
      async function fetchQuiz(){
-        const response=await fetch(` 'https://quizapi.io/api/v1/questions?apiKey=x2lv8eYb5NIPZe8E3rYIEHYgVjHoA4hLeUsoP0ly&limit=10&tags=HTML'`)
+        try{
+          const response = await fetch(`https://quizapi.io/api/v1/questions?apiKey=x2lv8eYb5NIPZe8E3rYIEHYgVjHoA4hLeUsoP0ly&limit=10&tags=HTML`)
+
 const data=await response.json();
 setQuestions(data);
+        }catch(error){
+setError("Error fetching html Questions")
+        }
     }
     useEffect(()=>{
      fetchQuiz();
@@ -18,6 +27,16 @@ setQuestions(data);
     return(
         <div className="game-parent-container">
       <h1 className="game-title">HTML QUIZ CHALLENGE</h1>
+      
+        <div className="current-question">
+        {questions.length===0?(
+        <p>loading questions....</p>
+      ) :(
+        <p>{currentQuestionIndex+1}:{questions[currentQuestionIndex].question}</p>
+      )}
+
+        </div>
+     
 
         </div>
     )
