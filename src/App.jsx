9e6fbd6a5bc86c32948e49fb59a8fee237482563
Naominm <naomimbugua536@gmail.com>
2025-03-17 11:23:50 +0300

    initial project reset

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Game from "./components/Game";
+function App(){
+  return(
+    <>
+    <h1>Common HTML Interview Questions</h1>
+    <Game/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Game.jsx
@@ -0,0 +1,12 @@
+
+import "./Game.css";
+
+function Game(){
+    return(
+        <>
+        <p>here goes the game api logic</p>
+        </>
+    )
+}
+
+export default Game;
