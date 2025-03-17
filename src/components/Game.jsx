a966457f72716a816f7c58c53db419fa74d05346
Naominm <naomimbugua536@gmail.com>
2025-03-17 11:43:01 +0300

    Billd the title section

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,11 +1,25 @@
 
+import { useState,useEffect } from "react";
+
 import "./Game.css";
 
 function Game(){
+    const[questions,setQuestions]=useState([]);
+
+     async function fetchQuiz(){
+        const response=await fetch(` 'https://quizapi.io/api/v1/questions?apiKey=x2lv8eYb5NIPZe8E3rYIEHYgVjHoA4hLeUsoP0ly&limit=10&tags=HTML'`)
+const data=await response.json();
+setQuestions(data);
+    }
+    useEffect(()=>{
+     fetchQuiz();
+    },[])
+
     return(
-        <>
-        <p>here goes the game api logic</p>
-        </>
+        <div className="game-parent-container">
+      <h1 className="game-title">HTML QUIZ CHALLENGE</h1>
+
+        </div>
     )
 }
 
